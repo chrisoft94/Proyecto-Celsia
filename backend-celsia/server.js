78bf22fc8e5b607d5493105ee1b20d54061141f5
Christian Porras <68763230+chrisoft94@users.noreply.github.com>
2025-10-30 13:09:49 -0500

    Refactor server to use consistent port variable

--- backend-celsia/server.js
+++ backend-celsia/server.js
@@ -14,6 +14,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 // 🔹 Array temporal para almacenar los usuarios registrados
 const usuarios = [];
+const port = process.env.PORT || 80;
 
 /**
  * @route   POST /api/auth/register
@@ -57,5 +58,7 @@ app.get('/', (req, res) => {
     res.send('Hello World\nWelcome to Seenode!');
 });
 
-const PORT = process.env.PORT || 80;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
